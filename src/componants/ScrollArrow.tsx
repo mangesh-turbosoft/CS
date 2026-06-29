f1daa85f08b3cc0eb043157fc6c10d290886938c
mangesh-turbosoft <mangesh@turbosoft.uk>
2026-06-29 11:44:44 +0530

    done

--- src/componants/ScrollArrow.tsx
+++ src/componants/ScrollArrow.tsx
@@ -1,26 +1,12 @@
 "use client";
 
-import Image from "next/image";
-
 export default function ScrollArrow() {
   return (
     <div
-      onClick={() => {
-        (window as any).goToSection?.(1);
-      }}
-      className="
-        hidden desktop:block
-        bottom-[0px]
-        left-1/2
-        -translate-x-1/2
-        w-[30px]
-        h-[30px]
-        z-[999]
-        animate-bounce
-        cursor-pointer
-      "
+      onClick={() => document.getElementById("next")?.scrollIntoView({ behavior: "smooth" })}
+      className="hidden desktop:block absolute bottom-6 md:bottom-10 left-1/2 -translate-x-1/2 w-[30px] h-[30px] z-[999] animate-bounce cursor-pointer"
     >
-      <Image
+      <img
         src="/images/inner-down-arrow.png"
         alt="Scroll Down"
         width={30}
