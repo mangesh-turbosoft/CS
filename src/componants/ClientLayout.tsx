593171e114e8c885ba3b991b8df3b0339964c6dd
mangesh-turbosoft <mangesh@turbosoft.uk>
2026-06-29 11:53:47 +0530

    done

--- src/componants/ClientLayout.tsx
+++ src/componants/ClientLayout.tsx
@@ -1,32 +1,9 @@
 "use client";
 
-import { useEffect, useState } from "react";
-
 export default function ClientLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const hasVisited = sessionStorage.getItem("loaderShown");
-
-    if (!hasVisited) {
-      setLoading(true);
-
-      const timer = setTimeout(() => {
-        setLoading(false);
-        sessionStorage.setItem("loaderShown", "true");
-      }, 1800);
-
-      return () => clearTimeout(timer);
-    }
-  }, []);
-
-  return (
-    <>
-       {children}
-    </>
-  );
+  return <>{children}</>;
 }
