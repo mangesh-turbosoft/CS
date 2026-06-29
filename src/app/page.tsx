5b06ed78dcf479f18d73f066716a9ca84a673278
mangesh-turbosoft <mangesh@turbosoft.uk>
2026-06-29 12:54:23 +0530

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,21 @@
 "use client";
 
 import Footer from "@/componants/Footer";
-import Firstsec from "@/componants/Firstsec";
 import Intro from "@/componants/Intro";
 import CaseStudies from "@/componants/casestudies";
 import ServicesAnimation from "@/componants/ServicesAnimation";
 import FullPageScroll from "@/componants/FullPageScroll";
+import PageHero from "@/componants/pageHero";
 
 export default function Home() {
   return (
     <>
       <FullPageScroll />
       <ServicesAnimation />
-      <Firstsec />
+      <PageHero
+        title={<>Web <br /> Engineering</>}
+        subtitle="Premium websites in Headless, HubSpot and WordPress."
+      />
       <Intro />
     <CaseStudies />
 
